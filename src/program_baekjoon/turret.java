9c701a2e0f33237b782863509457f450d50789cf
=박다희 <jwp9622@naver.com>
2025-05-27 11:53:56 +0900

    2025-05-2_5

---
+++ src/program_baekjoon/turret.java
@@ -0,0 +1,37 @@
+package program_baekjoon;
+
+import java.util.Scanner;
+
+public class plus {
+	
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int result= 0;
+        while(a>0){
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int r1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            int r2 = sc.nextInt();
+            result = point(x1,y1,r1,x2,y2,r2);
+            System.out.println(result);
+            a--;
+        }
+    }
+    public static int point(int x1, int y1, int r1, int x2, int y2, int r2){
+        double d = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        System.out.println("____");
+        if(d ==0&& r1==r2){ 
+            return -1; //무한이 많은 접점(동일한 원)
+        }else if(d==r1+r2 || d == Math.abs(r1-r2)){
+            return 1; //원이 외접, 내접
+        }else if( Math.abs(r1-r2)<d && d<r1+r2 ){
+            return 2;
+        }else{
+            return 0;
+        }
+	}
+    
+}
